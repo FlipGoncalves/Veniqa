9c349894ea0cccd84ce1e05d6f81fc6ea7bf7253
Filipe Gonçalves <filipeg@ua.pt>
2024-04-25 16:17:42 +0100

    tidy files and misssing env secret in redis

--- deployment/secrets.sh
+++ deployment/secrets.sh
@@ -27,7 +27,3 @@ kubectl create secret generic management-server-secret \
 kubectl create secret generic redis-secret \
     --from-literal=REDIS_REPLICATION_MODE=master \
     -n gic-asenhoradosaneis
-
-kubectl create secret generic mysql-pass \
-    --from-literal=password=YOUR_PASSWORD \
-    -n gic-asenhoradosaneis
