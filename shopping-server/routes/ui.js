207aeded30539f305c1acb2d4af91c3eb0022eda
SrPhoenix <vicente.costa@ua.pt>
2024-06-03 15:14:32 +0100

    cors try fix

--- shopping-server/routes/ui.js
+++ shopping-server/routes/ui.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import passportAuth from '../authentication/passportAuth';
 import uiController from '../controllers/uiController.js';
+import cors from 'cors';
 var router = express.Router();
 
+
+
+// To Allow cross origin requests originating from selected origins
+var corsOptions = {
+    origin: "*",
+    methods: ['GET, POST, OPTIONS, PUT, DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false
+  }
+
+  
+  
 /* GET UI Customziations Endpoint. */
-router.get('/', function(req, res, next) {
+router.get('/', cors(corsOptions), function(req, res, next) {
     res.render('index', { title: 'Veniqa User Interface Customizations' });
 });
 
-router.get('/featured', uiController.getFeaturedSection);
+router.get('/featured', cors(corsOptions), uiController.getFeaturedSection);
 
-router.get('/productCategoryList', uiController.getProductCategoryList);
+router.get('/productCategoryList', cors(corsOptions), uiController.getProductCategoryList);
 
 module.exports = router;
